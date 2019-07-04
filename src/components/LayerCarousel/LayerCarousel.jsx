f2084f0325d4860e874ce44e54901cd2831abe08
eb <elad705@gmail.com>
2019-07-04 16:08:13 +0300

    Layers first design

--- src/components/LayerCarousel/LayerCarousel.jsx
+++ src/components/LayerCarousel/LayerCarousel.jsx
@@ -1,32 +1,83 @@
-import Carousel from 'react-native-snap-carousel';
 import React, {Component} from 'react';
+import Slider from 'react-slick';
+//import './Layer.scss';
 
 class MyCarousel extends Component {
 
     constructor(props) {
         super(props);
 
-
-        this.renderItem = this.renderItem.bind(this);
+        this.state = {
+            nav1: null,
+            nav2: null
+        };
     }
 
-    renderItem({item, index}) {
-        return (
-            <div>
-                <h1>{item.title}</h1>
-                <h3>{item.desc}</h3>
-            </div>
-        );
+    componentDidMount() {
+        this.setState({
+            nav1: this.slider1,
+            nav2: this.slider2
+        });
     }
+
 
     render() {
-        return (<Carousel   
-                    data={[{title:"bla", desc:"bla bla"}]}
-                    renderItem={this.renderItem}
-                    itemWidth={50}
-                    sliderWidth={150}
-                    loop={true}
-                    autoplay={true}/>);
+        const settings = {
+           slidesToShow:3,
+           dots: true,
+           centerMode: true,
+           focusOnSelect: true,
+           swipeToSlide:true,
+        };
+
+        const presontorSettings = {
+            slidesToShow:1,
+            swipeToSlide:false,
+            focusOnSelect:true,
+            arrows:false,
+            draggable: false
+        }
+
+        const layers = (
+            <span>
+                <div><h3>1</h3></div>
+                <div><h3>2</h3></div>
+                <div><h3>3</h3></div>
+                <div><h3>4</h3></div>
+                <div><h3>5</h3></div>
+                <div><h3>6</h3></div>
+                <div><h3>7</h3></div>
+            </span>
+        );
+
+        
+        return (
+            <div className="layersContainer">
+                <Slider asNavFor={this.state.nav2}
+                        ref={slider => this.slider1 = slider}
+                        {...presontorSettings}>
+                        <div><h3>1</h3></div>
+                        <div><h3>2</h3></div>
+                        <div><h3>3</h3></div>
+                        <div><h3>4</h3></div>
+                        <div><h3>5</h3></div>
+                        <div><h3>6</h3></div>
+                        <div><h3>7</h3></div>
+                </Slider>
+                <Slider asNavFor={this.state.nav1}
+                        ref={slider => this.slider2 = slider}
+                        {...settings}>
+                        <div><h3>1</h3></div>
+                        <div><h3>2</h3></div>
+                        <div><h3>3</h3></div>
+                        <div><h3>4</h3></div>
+                        <div><h3>5</h3></div>
+                        <div><h3>6</h3></div>
+                        <div><h3>7</h3></div>
+                </Slider>
+            </div>
+            
+        );
     }
     
 }
