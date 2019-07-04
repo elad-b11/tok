affae87b2e3a196c3eb56ab3252b6f421c775dab
eb <elad705@gmail.com>
2019-07-04 16:05:55 +0300

    Fix webpack eot files loading

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -27,8 +27,7 @@ export default {
             {test: /\.jsx?$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
             {test: /(\.css)$/, loaders: ['style', 'css']},
             {test: /\.(sass|scss)$/, loaders: ['style', 'css','sass']},
-            {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-            {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
+            {test: /\.(woff|woff2|eot)$/, loader: "url?prefix=font/&limit=5000"},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
             {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"},
             {test: /\.(gif|png|jpe?g)$/i, loader: "url-loader"}            
