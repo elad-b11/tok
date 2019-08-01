991e438339bd180c43a57c0ead09d7a104fd300a
eb <elad705@gmail.com>
2019-08-01 09:52:56 +0300

    Add icons to table

--- src/components/modelTabs/fieldsTab/fieldsTab.jsx
+++ src/components/modelTabs/fieldsTab/fieldsTab.jsx
@@ -1,18 +1,20 @@
-import React, {Component} from 'react';
+import React, {Component, forwardRef} from 'react';
 import MaterialTable from 'material-table';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import SaveAlt from '@material-ui/icons/SaveAlt';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
 import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
 import FirstPage from '@material-ui/icons/FirstPage';
 import LastPage from '@material-ui/icons/LastPage';
-import Add from '@material-ui/icons/Add';
-import Check from '@material-ui/icons/Check';
-import FilterList from '@material-ui/icons/FilterList';
 import Remove from '@material-ui/icons/Remove';
-import {SvgIconProps} from '@material-ui/core/SvgIcon';
-
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const columns = [
     {title: 'חובה', field: 'required', type:'boolean'},
@@ -67,8 +69,28 @@ class FieldsTab extends Component {
     }
 
     render() {
-        React.createElement(SvgIconProps,)
+        const tableIcons = {
+            Add: forwardRef((props, ref) => <AddBox {...props} ref={ref}/>),
+            Check: forwardRef((props, ref) => <Check {...props} ref={ref}/>),
+            Clear: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+            Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref}/>),
+            DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+            Edit: forwardRef((props, ref) => <Edit {...props} ref={ref}/>),
+            Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref}/>),
+            Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref}/>),
+            FirstPage: forwardRef((props, ref) => <LastPage  {...props} ref={ref}/>),
+            LastPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref}/>),
+            NextPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref}/>),
+            PreviousPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref}/>),
+            ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref}/>),
+            Search: forwardRef((props, ref) => <Search {...props} ref={ref}/>),
+            SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref}/>),
+            ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref}/>),
+            ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref}/>)
+        };
+
         return (<MaterialTable 
+                    style={{direction: 'rtl'}}
                     columns={this.state.columns}
                     data={this.state.data}
                     editable={{
@@ -76,27 +98,9 @@ class FieldsTab extends Component {
                         onRowUpdate: this.onRowUpdate,
                         onRowDelete: this.onRowDelete
                     }}
-                    icons={{
-                        Check: <Check/>
-                    }}
-
+                    icons={tableIcons}
                 />);
     }
 }
-
-
-// icons={{
-//     Check: () => <Check/>,
-//     Export: () => <SaveAlt/>,
-//     Filter: () => <FilterList />,
-//     FirstPage: () => <FirstPage/>,
-//     LastPage: () =>  <LastPage/>,
-//     NextPage: () => <ChevronRight/>,
-//     PreviousPage: () => <ChevronLeft/>,
-//     Search: ()=> <Search/>,
-//     ThirdStateCheck: () => <Remove/>,
-//     ViewColumn: () => <ViewColumn/>,
-//     DetailPanel:() => <ChevronRight/>
-//}}
 
 export default FieldsTab;
