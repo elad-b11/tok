039e36db27132e3a7b03869f7936912f47a3be76
Unknown <peerofir@gmail.com>
2019-08-24 22:20:17 +0300

    change to a clearer source map

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,37 +1,47 @@
-import webpack from 'webpack';
-import path from 'path';
+import webpack from "webpack";
+import path from "path";
 
 export default {
-    debug: true,
-    devtool: 'cheap-module-eval-source-map',
-    noInfo: false,
-    entry: [
-        'webpack-hot-middleware/client?reload=true',
-        'babel-polyfill', 
-        './src/app.jsx'
-    ],
-    target: 'web',
-    output: {
-        path: __dirname + '/dist', 
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './src'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ],
-    module: {
-        loaders: [            
-            {test: /\.jsx?$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-            {test: /(\.css)$/, loaders: ['style', 'css']},
-            {test: /\.(sass|scss)$/, loaders: ['style', 'css','sass']},
-            {test: /\.(woff|woff2|eot)$/, loader: "url?prefix=font/&limit=5000"},
-            {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"},
-            {test: /\.(gif|png|jpe?g)$/i, loader: "url-loader"}            
-        ]
-    }
-}; 
+  debug: true,
+  devtool: "eval-source-map",
+  noInfo: false,
+  entry: [
+    "webpack-hot-middleware/client?reload=true",
+    "babel-polyfill",
+    "./src/app.jsx"
+  ],
+  target: "web",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./src"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, "src"),
+        loaders: ["babel"]
+      },
+      { test: /(\.css)$/, loaders: ["style", "css"] },
+      { test: /\.(sass|scss)$/, loaders: ["style", "css", "sass"] },
+      { test: /\.(woff|woff2|eot)$/, loader: "url?prefix=font/&limit=5000" },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      },
+      { test: /\.(gif|png|jpe?g)$/i, loader: "url-loader" }
+    ]
+  }
+};
